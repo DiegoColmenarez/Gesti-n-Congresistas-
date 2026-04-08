686e0e9d6052af6e95595d1a5599436a3b88b5b6
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-04-08 01:00:42 -0500

    correccion gitignore

---
+++ src/main/java/com/colmenarez/softwareCongresistas/domain/exceptions/InvalidInstitucionException.java
@@ -0,0 +1,7 @@
+package com.colmenarez.softwareCongresistas.domain.exceptions;
+
+public class InvalidInstitucionException extends RuntimeException {
+    public InvalidInstitucionException(String message) {
+        super(message);
+    }
+}
